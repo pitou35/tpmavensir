6d0080ae8ad5e85bf1ac4fd529c1cc903e02b21b
15002774 <15002774@e010m10.istic.univ-rennes1.fr>
2016-01-06 15:13:07 +0100

    initial commit

---
+++ tpmaven/src/main/java/fr/istic/sir/tpmaven/App.java
@@ -0,0 +1,18 @@
+package fr.istic.sir.tpmaven;
+
+/**
+ * Hello world!
+ * voici une licorne bleue
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    private int test(){
+    	return 1;
+    }
+}
